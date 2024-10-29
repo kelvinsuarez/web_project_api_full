548f0835d9e704a7fe7471e689aad987070895b8
Kelvin Suarez <k_suarez22@hotmail.com>
2024-10-29 08:42:05 -0400

    redirigiendo BASE_URL a la api de mi servidor

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://tripleten.desarrollointerno.com";
+const BASE_URL = "https://api.p18.ignorelist.com";
 
 export const register = ({email, password}) => {
     return fetch(`${BASE_URL}/signup`, {
