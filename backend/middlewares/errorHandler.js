292f81273f9088418aff80531a959d9d26e02810
Kelvin Suarez <k_suarez22@hotmail.com>
2026-03-18 12:45:50 -0400

    actualizando el manejador de errores para enviar mensajes predeterminados

--- backend/middlewares/errorHandler.js
+++ backend/middlewares/errorHandler.js
@@ -1,9 +1,17 @@
+const { isCelebrateError } = require('celebrate');
 const { HttpStatus, HttpResponseMessage } = require('../enums/http');
 
 module.exports = (err, req, res, next) => {
   console.error('🧨 Error capturado en middleware:', err);
 
+  if (isCelebrateError(err)) {
+    return res.status(HttpStatus.BAD_REQUEST).send({
+      message: HttpResponseMessage.BAD_REQUEST,
+    });
+  }
+
   const { statusCode = HttpStatus.INTERNAL_SERVER_ERROR, message } = err;
+
   res.status(statusCode).send({
     message:
       statusCode === HttpStatus.INTERNAL_SERVER_ERROR
